444d0f35c51f839334b846df425b388eed378122
nansmaurel <nans05@live.fr>
2023-02-02 14:47:48 +0100

    Petit questionnaire terminé

--- app/src/main/java/com/example/genepi/MainActivity.kt
+++ app/src/main/java/com/example/genepi/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.genepi
 
+import android.media.Rating
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,9 +18,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var textFirstName = ""
+        var ratingKotlin = 0
+        var userHapiness = 0
 
         var toastButton = findViewById<Button>(R.id.main_button_toast)
         var firstnameEditText = findViewById<EditText>(R.id.main_edittext_firstname)
+        var ratingBar = findViewById<RatingBar>(R.id.main_ratingBar)
+        var seekBar = findViewById<SeekBar>(R.id.main_seekBar)
         var formButton = findViewById<Button>(R.id.main_button_form)
 
         formButton.isEnabled = false;
@@ -32,6 +39,20 @@ class MainActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
                 textFirstName = s.toString()
                 formButton.isEnabled = s.toString().isNotEmpty()
+            }
+        })
+
+        ratingBar.setOnRatingBarChangeListener(object : RatingBar.OnRatingBarChangeListener {
+            override fun onRatingChanged(ratingBar: RatingBar?, rating: Float, fromUser: Boolean) {
+                ratingKotlin = rating.toInt()
+            }
+        })
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?){}
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                userHapiness = progress
             }
         })
 
