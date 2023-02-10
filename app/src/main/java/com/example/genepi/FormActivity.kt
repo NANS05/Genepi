0656eb264cc1a09a70d65f82afe88368c3815021
nansmaurel <nans05@live.fr>
2023-02-10 11:47:19 +0100

    Mise en forme du menu et des icones

--- app/src/main/java/com/example/genepi/FormActivity.kt
+++ app/src/main/java/com/example/genepi/FormActivity.kt
@@ -29,7 +29,7 @@ class FormActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_questions
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
